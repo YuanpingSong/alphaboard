66d0298ad1c30ef26001410975bbaa0df7630bf2
Yuanping Song <yuanpingsong@Yuanpings-MacBook-Pro.local>
2019-03-30 14:03:28 -0700

    import highcharts

--- alpha_ui/src/App.vue
+++ alpha_ui/src/App.vue
@@ -8,6 +8,10 @@
 <script>
 import Finance from './components/Finance.vue'
 import News from './components/News.vue'
+import Vue from 'vue'
+import HighchartsVue from 'highcharts-vue'
+Vue.use(HighchartsVue)
+
 
 
 export default {
