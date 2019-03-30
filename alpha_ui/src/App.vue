1570cff1172dad519045c9b80d82861bd4ef6b7c
Yuanping Song <yuanping.song@outlook.com>
2019-03-30 15:38:28 -0700

    new feature

--- alpha_ui/src/App.vue
+++ alpha_ui/src/App.vue
@@ -1,9 +1,46 @@
 <template>
   <div id="app">
-      <Finance></Finance>
-      <News></News>
 
-      <v-jumbotron>
+      <v-card
+              color="grey lighten-4"
+              flat
+              height="200px"
+              tile
+      >
+          <v-toolbar dense>
+              <v-toolbar-side-icon></v-toolbar-side-icon>
+
+              <v-toolbar-title>Title</v-toolbar-title>
+
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                  <v-icon>search</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                  <v-icon>favorite</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                  <v-icon>more_vert</v-icon>
+              </v-btn>
+          </v-toolbar>
+      </v-card>
+
+      <!--<v-img src="webwxgetmsgimg.jpeg" max-height="1000" max-width="1000">-->
+          <!--<v-layout-->
+                  <!--align-center-->
+                  <!--column-->
+                  <!--justify-center-->
+          <!--&gt;-->
+              <!--<h1 class="display-2 font-weight-thin mb-3">Vuetify.js</h1>-->
+              <!--<h4 class="subheading">Build your application today!</h4>-->
+          <!--</v-layout>-->
+      <!--</v-img>-->
+
+
+      <v-responsive>
           <v-container fill-height>
               <v-layout align-center>
                   <v-flex>
@@ -25,7 +62,11 @@
                   </v-flex>
               </v-layout>
           </v-container>
-      </v-jumbotron>
+      </v-responsive>
+
+
+      <Finance></Finance>
+      <News></News>
 
 
   </div>
@@ -54,5 +95,9 @@ export default {
 </script>
 
 <style>
+    #inspire img {
+        height: 100%
+    }
+
 
 </style>
--- alpha_ui/src/main.js
+++ alpha_ui/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import HighchartsVue from 'highcharts-vue'
+// index.js or main.js
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 Vue.use(HighchartsVue)
 
 Vue.config.productionTip = false
