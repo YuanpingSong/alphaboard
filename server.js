fa9a141e49250235aa8bde37b084db7a97088c29
Yuanping Song <yuanpingsong@Yuanpings-MacBook-Pro.local>
2019-03-30 00:21:36 -0700

    initialize server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send("OK")
+});
+
+// use port 8080 in your local environment. ports bellow 1024 requires sudo 
+app.listen(8080, function () {
+    console.log('Server listening on port 3000');
+});
