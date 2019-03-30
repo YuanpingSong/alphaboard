5fe652babfc2d7522310b1908a57c477aa44904b
Yuanping Song <yuanping.song@outlook.com>
2019-03-30 14:30:00 -0700

    djfklz;

--- alpha_ui/src/App.vue
+++ alpha_ui/src/App.vue
@@ -2,14 +2,45 @@
   <div id="app">
       <Finance></Finance>
       <News></News>
+
+      <v-jumbotron>
+          <v-container fill-height>
+              <v-layout align-center>
+                  <v-flex>
+                      <h3 class="display-3">Welcome to the site</h3>
+
+                      <span class="subheading">Lorem ipsum dolor sit amet, pri veniam forensibus id. Vis maluisset molestiae id, ad semper lobortis cum. At impetus detraxit incorrupte usu, repudiare assueverit ex eum, ne nam essent vocent admodum.</span>
+
+                      <v-divider class="my-3"></v-divider>
+
+                      <div class="title mb-3">Check out our newest features!</div>
+
+                      <v-btn
+                              class="mx-0"
+                              color="primary"
+                              large
+                      >
+                          See more
+                      </v-btn>
+                  </v-flex>
+              </v-layout>
+          </v-container>
+      </v-jumbotron>
+
+
   </div>
 </template>
+
+
 
 <script>
 import Finance from './components/Finance.vue'
 import News from './components/News.vue'
 import Vue from 'vue'
 import HighchartsVue from 'highcharts-vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
 Vue.use(HighchartsVue)
 
 
