d9530713418391f961f98e5f328e1c7423202b6c
Yuanping Song <yuanpingsong@Yuanpings-MacBook-Pro.local>
2019-03-30 12:41:36 -0700

    initialized directory structure

--- alpha_ui/src/App.vue
+++ alpha_ui/src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <Fiance></Fiance>
+      <News></News>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Finance from './components/Finance.vue'
+import News from './components/News.vue'
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Finance, News
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ alpha_ui/src/components/Finance.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="Finance">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Finance',
+        props: {
+            // msg: String
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
